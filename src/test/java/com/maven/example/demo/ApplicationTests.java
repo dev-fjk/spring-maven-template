1415e82324fa36b7c3eafadfb18d325e9a41132d
dev-fjk <fjk.develop@gmail.com>
2021-10-14 00:24:17 +0900

    spockのバージョンアップに伴う修正
    - 業務で滅茶苦茶ハマったので調査も兼ねて 出来るだけ最小限の構成に変更した。
        - PJ名などを maven-base から spring-maven-template へと修正 メインクラス名を汎用的な名称に変更
    - maven compileのステップ記載を削除
    - maven-surefire-plugin のバージョンを最新に変更
        - 上記に伴い surefire-junit47を定義している場合　spock2.0が動かない事象を検出したため、　junit platformをsurefireのpluginに追加
    - デフォルトをMySQLの使用からPostgreSQLの使用に変更

--- src/test/java/com/maven/example/demo/ApplicationTests.java
+++ src/test/java/com/maven/example/demo/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MavenBaseApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
